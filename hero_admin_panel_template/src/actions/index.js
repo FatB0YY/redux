ce37101eaf53f04d52f2ac90d23c3c6c4ca15829
rodion <rodion.rv111@icloud.com>
2022-01-17 16:18:25 +0300

    adding functionality inside the reducer

--- hero_admin_panel_template/src/actions/index.js
+++ hero_admin_panel_template/src/actions/index.js
@@ -1,3 +1,4 @@
+// загрузка героев
 export const heroesFetching = () => {
   return {
     type: 'HEROES_FETCHING',
@@ -14,5 +15,48 @@ export const heroesFetched = (heroes) => {
 export const heroesFetchingError = () => {
   return {
     type: 'HEROES_FETCHING_ERROR',
+  }
+}
+
+// загрузка фильтров
+export const filtersFetching = () => {
+  return {
+    type: 'FILTERS_FETCHING',
+  }
+}
+
+export const filtersFetched = (filters) => {
+  return {
+    type: 'FILTERS_FETCHED',
+    payload: filters,
+  }
+}
+
+export const filtersFetchingError = () => {
+  return {
+    type: 'FILTERS_FETCHING_ERROR',
+  }
+}
+
+// управление героями
+export const heroDeleted = (id) => {
+  return {
+    type: 'HERO_DELETED',
+    payload: id,
+  }
+}
+
+export const heroCreated = (hero) => {
+  return {
+    type: 'HERO_CREATED',
+    payload: hero,
+  }
+}
+
+// управление фильтрами
+export const activeFilterChanged = (filter) => {
+  return {
+    type: 'ACTIVE_FILTER_CHANGED',
+    payload: filter,
   }
 }
--- hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
+++ hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
@@ -1,4 +1,12 @@
+import { useEffect, useCallback, useState } from 'react'
+import { useHttp } from '../../hooks/http.hook'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { heroCreated } from '../../actions'
+
+import Spinner from '../spinner/Spinner'
+
+import { v4 as uuidv4 } from 'uuid'
 
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
@@ -11,48 +19,117 @@
 // данных из фильтров
 
 const HeroesAddForm = () => {
-    return (
-        <form className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
+  // Состояния для контроля формы
+  const [heroName, setHeroName] = useState('')
+  const [heroDescr, setHeroDescr] = useState('')
+  const [heroElement, setHeroElement] = useState('')
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
+  const { filters, filtersLoadingStatus } = useSelector((state) => state)
+  const { request } = useHttp()
+  const dispatch = useDispatch()
 
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
-                </select>
-            </div>
+  if (filtersLoadingStatus === 'loading') {
+    return <Spinner />
+  } else if (filtersLoadingStatus === 'error') {
+    return <h5 className='text-center mt-5'>Ошибка загрузки</h5>
+  }
 
-            <button type="submit" className="btn btn-primary">Создать</button>
-        </form>
-    )
+  const renderFiltersList = (arr) => {
+    if (arr.length === 0 || !arr) {
+      return <h5 className='text-center mt-5'>Фильтров пока нет</h5>
+    }
+
+    return arr.map(({ name }) => {
+      if (name === 'all') return
+
+      return <option key={name}>{name}</option>
+    })
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+
+    const newHero = {
+      id: uuidv4(),
+      name: heroName,
+      description: heroDescr,
+      element: heroElement,
+    }
+
+    // Отправляем данные на сервер в формате JSON
+    // ТОЛЬКО если запрос успешен - отправляем персонажа в store
+    request('http://localhost:3001/heroes', 'POST', JSON.stringify(newHero))
+      .then((res) => console.log(res, 'Отправка успешна'))
+      .then(dispatch(heroCreated(newHero)))
+      .catch((err) => console.log(err))
+
+    // Очищаем форму после отправки
+    setHeroName('')
+    setHeroDescr('')
+    setHeroElement('')
+  }
+
+  const filtersList = renderFiltersList(filters)
+
+  return (
+    <form
+      className='border p-4 shadow-lg rounded'
+      onSubmit={(event) => onSubmit(event)}
+    >
+      <div className='mb-3'>
+        <label htmlFor='name' className='form-label fs-4'>
+          Имя нового героя
+        </label>
+        <input
+          required
+          type='text'
+          name='name'
+          className='form-control'
+          id='name'
+          placeholder='Как меня зовут?'
+          value={heroName}
+          onChange={(event) => setHeroName(event.target.value)}
+        />
+      </div>
+
+      <div className='mb-3'>
+        <label htmlFor='text' className='form-label fs-4'>
+          Описание
+        </label>
+        <textarea
+          required
+          name='text'
+          className='form-control'
+          id='text'
+          placeholder='Что я умею?'
+          style={{ height: '130px' }}
+          value={heroDescr}
+          onChange={(event) => setHeroDescr(event.target.value)}
+        />
+      </div>
+
+      <div className='mb-3'>
+        <label htmlFor='element' className='form-label'>
+          Выбрать элемент героя
+        </label>
+        <select
+          required
+          className='form-select'
+          id='element'
+          name='element'
+          value={heroElement}
+          onChange={(e) => setHeroElement(e.target.value)}
+        >
+          <option value=''>Я владею элементом...</option>
+          {filtersList}
+        </select>
+      </div>
+
+      <button type='submit' className='btn btn-primary'>
+        Создать
+      </button>
+    </form>
+  )
 }
 
-export default HeroesAddForm;
+export default HeroesAddForm
